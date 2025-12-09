10f045e770f4c3b9384cf7e6581afaf42963139d
Paridhi0502 <paridhi05092005@gmail.com>
2025-12-10 05:09:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC2Q3vLbwp7xM-FRhDXUxFrkVMDEzQtLfY",
+  authDomain: "car-accident-detection-s-5b09c.firebaseapp.com",
+  projectId: "car-accident-detection-s-5b09c",
+  storageBucket: "car-accident-detection-s-5b09c.firebasestorage.app",
+  messagingSenderId: "26502814894",
+  appId: "1:26502814894:web:ce9c12c22f737b0648213f",
+  measurementId: "G-GH7EEQQ2S5"
+};
